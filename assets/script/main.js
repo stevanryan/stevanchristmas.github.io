2838e56d05b6549d068cd8276367251aa849b41a
stevanryan <stevanryan85@gmail.com>
2022-12-25 22:30:23 +0700

    first commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,10 +1,32 @@
 const run = document.querySelector('.welcome');
 run.addEventListener('click', function() {
     run.classList.add('active');
+    // music();
 });
 
 const snowOn = document.querySelector('#on');
-const header = document.querySelector('.head');
+const main = document.querySelector('.snowy-background');
 snowOn.addEventListener('click', function() {
-    header.classList.toggle('headerActive');
+    main.classList.toggle('snowyActive');
 });
+
+// snowOn.addEventListener('click', function() {
+//     music();
+// });
+
+const music = new Audio();
+music.src = "assets/audio/christmas-magic-night-11648.mp3";
+music.loop = true;
+
+snowOn.addEventListener('click', function() {
+    musicPlay();
+});
+
+function musicPlay() {
+    if(music.paused) {
+        music.play();
+    } else {
+        music.pause();
+        music.currentTime = 0;
+    }
+}
